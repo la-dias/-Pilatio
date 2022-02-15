bc0f7f18e64f8839175fd7b5458f2f7f2ab3e4c7
Lucas <76748381+la-dias@users.noreply.github.com>
2022-02-15 18:42:14 -0300

    Projeto inicial
    HTML, CSS e JS (Apenas menu Hamburger)

---
+++ script.js
@@ -0,0 +1,21 @@
+/*Menu Hamburger*/
+
+const btnBurger = document.getElementById('btn-burger');
+
+function toggleMenu(event) {
+  if (event.type === 'touchstart') event.preventDefault();
+  const nav = document.getElementById('nav');
+  nav.classList.toggle('active');
+  const active = nav.classList.contains('active');
+  event.currentTarget.setAttribute('aria-expanded', active);
+  if (active) {
+    event.currentTarget.setAttribute('aria-label', 'Fechar Menu');
+  } else {
+    event.currentTarget.setAttribute('aria-label', 'Abrir Menu');
+  }
+}
+
+btnBurger.addEventListener('click', toggleMenu);
+btnBurger.addEventListener('touchstart', toggleMenu);
+
+/* Comentários */
